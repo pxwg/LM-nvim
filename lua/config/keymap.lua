4cd27de516192982d63f27dd7a1c0af04a1cd5f2
向昕宇 <pxwg-dogggie@xiangxinyudeMacBook-Pro.local>
2025-01-10 14:01:03 +0800

    share clipboard and better keymap

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,17 +1,83 @@
 local map = vim.keymap.set
+local cn = require("utils.autocorrect")
 
--- windows
-map("n", "<C-h>", "<C-w>h", { silent = true })
+local function save_and_delete_last_line()
+  local ft = vim.bo.filetype
+  local cursor_pos = vim.fn.getpos(".") -- 记录光标位置
+
+  if ft == "tex" or ft == "markdown" then
+    vim.cmd("w")
+    local view = vim.fn.winsaveview()
+    vim.api.nvim_buf_set_lines(0, -2, -1, false, {})
+    vim.fn.winrestview(view)
+    cn.autocorrect()
+    vim.cmd("w")
+  else
+    vim.cmd("w")
+  end
+  vim.fn.setpos(".", cursor_pos) -- 恢复光标位置
+end
+-- windows with hammerspoon function
 map("n", "<C-j>", "<C-w>j", { silent = true })
 map("n", "<C-k>", "<C-w>k", { silent = true })
-map("n", "<C-l>", "<C-w>l", { silent = true })
+local function is_rightmost_window()
+  local current_win = vim.api.nvim_get_current_win()
+  local current_pos = vim.api.nvim_win_get_position(current_win)[2]
+  local max_col = current_pos
 
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local pos = vim.api.nvim_win_get_position(win)[2]
+    if pos > max_col then
+      max_col = pos
+    end
+  end
+
+  return current_pos == max_col
+end
+
+local function is_leftmost_window()
+  local current_win = vim.api.nvim_get_current_win()
+  local current_pos = vim.api.nvim_win_get_position(current_win)[2]
+  local min_col = current_pos
+
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local pos = vim.api.nvim_win_get_position(win)[2]
+    if pos < min_col then
+      min_col = pos
+    end
+  end
+
+  return current_pos == min_col
+end
+
+-- switch to right window
+map("n", "<C-l>", function()
+  if is_rightmost_window() then
+    vim.fn.system("hs -c 'focusNextWindow()'")
+  else
+    vim.cmd("wincmd l")
+  end
+end, { noremap = true, silent = true, desc = "Move to right window" })
+
+-- same for left
+map("n", "<C-h>", function()
+  if is_leftmost_window() then
+    vim.fn.system("hs -c 'focusPreviousWindow()'")
+  else
+    vim.cmd("wincmd h")
+  end
+end, { noremap = true, silent = true, desc = "Move to left window" })
 -- move lines
 map({ "n", "v" }, "L", "$", { silent = true })
 map({ "n", "v" }, "H", "0", { silent = true })
+map({ "n", "v" }, "j", "gj", { silent = true })
+map({ "n", "v" }, "k", "gk", { silent = true })
 
 --save
-map({ "n", "i", "v" }, "<C-s>", "<ESC>:w<CR>", { silent = true })
+map({ "n", "v", "i" }, "<C-s>", function()
+  save_and_delete_last_line()
+  vim.cmd("stopinsert")
+end, { noremap = true, silent = true })
 
 --jj to escape
 map("i", "jj", "<ESC>", { silent = true })
