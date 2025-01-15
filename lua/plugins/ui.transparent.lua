33730039c294d3b37d22bec5538dec01566a5a1e
向昕宇 <pxwg-dogggie@xiangxinyudeMacBook-Pro.local>
2025-01-15 17:24:43 +0800

    UI: add transparent mode

---
+++ lua/plugins/ui.transparent.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "tribela/transparent.nvim",
+    enabled = false,
+    cmd = "TransparentEnable",
+    config = true,
+  },
+  {
+    "xiyaowong/nvim-transparent",
+    event = "UIEnter",
+    config = true,
+  },
+}
