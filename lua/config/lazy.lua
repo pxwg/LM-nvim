9fbd46d2c88b773432c2dbb88c58d2c4ee306dd5
向昕宇 <pxwg-dogggie@xiangxinyudeMacBook-Pro.local>
2025-01-16 17:37:01 +0800

    add tab

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,9 @@ vim.g.maplocalleader = "\\"
 vim.g.picker = "telescope"
 vim.o.splitkeep = "screen"
 vim.opt.showmode = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 _G.mode = "error"
 
 -- Enable break indent
