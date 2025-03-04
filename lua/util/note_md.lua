7c7cb4db5a8a5381e4d54799f2191533d6c211cd
pxwg <xiangxy23@tsinghua.edu.cn>
2025-03-04 19:17:33 +0800

    add script for pasting relative path of md

---
+++ lua/util/note_md.lua
@@ -0,0 +1,65 @@
+local config = require("img-clip.config")
+local paste = require("img-clip.paste")
+
+local M = {}
+
+---@param config_opts? table
+M.setup = function(config_opts)
+  config.setup(config_opts)
+end
+
+---@param api_opts? table
+---@param input? string
+M.paste_image = function(api_opts, input)
+  config.api_opts = api_opts or {}
+  return paste.paste_image(input)
+end
+
+---@param api_opts? table
+---@param input? string
+M.pasteImage = function(api_opts, input)
+  config.api_opts = api_opts or {}
+  return paste.paste_image(input)
+end
+
+local function get_relative_path(file_path)
+  local current_buffer_path = vim.fn.expand("%:p:h")
+  local home_path = vim.fn.expand("~")
+
+  local relative_path = vim.fn.fnamemodify(file_path, ":~:.")
+
+  local common_prefix = vim.fn.fnamemodify(current_buffer_path, ":~:.")
+
+  if common_prefix == home_path then
+    return file_path
+  end
+
+  if relative_path:sub(1, #common_prefix) == common_prefix then
+    relative_path = relative_path:sub(#common_prefix + 2)
+  end
+
+  return "./" .. relative_path
+end
+
+local function generate_link(relative_path)
+  local file_name = vim.fn.fnamemodify(relative_path, ":t:r")
+  return string.format("[%s](%s)", file_name, relative_path)
+end
+
+local original_paste = vim.paste
+
+vim.paste = function(lines, phase)
+  if phase == -1 and vim.bo.filetype == "markdown" then
+    local file_path = lines[1]
+    if file_path:match("%.md$") then
+      local relative_path = get_relative_path(file_path)
+      local markdown_link = generate_link(relative_path)
+
+      vim.api.nvim_put({ markdown_link }, "l", true, true)
+      return true
+    end
+  end
+  return original_paste(lines, phase)
+end
+
+return M
