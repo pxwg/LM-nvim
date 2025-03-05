37a7269302b70abdece96716a8beae2c23e2a56e
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-03-05 17:54:23 +0800

    update for note writing

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -124,5 +124,3 @@ vim.cmd([[  function OpenMarkdownPreview (url)
     execute "silent ! open -a Firefox -n --args --new-window " . a:url
   endfunction
   let g:mkdp_browserfunc = 'OpenMarkdownPreview']])
-
-require("util.note_md")
