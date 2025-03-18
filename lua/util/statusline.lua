8b3e9f9a2df781f47e75a13b7fb5bb180dff1dde
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-03-19 03:17:29 +0800

    edit statusline

--- lua/util/statusline.lua
+++ lua/util/statusline.lua
@@ -1,6 +1,7 @@
 vim.o.statusline = table.concat({
   "%f", -- 文件名
   "%m", -- 修改标志
+  -- "%=%{%v:lua.require'util.current_function'.get_current_function()%}%=",
   "%=",
   "[" .. "en" .. "] ",
   require("util.battery").get_battery_icon() .. " ",
