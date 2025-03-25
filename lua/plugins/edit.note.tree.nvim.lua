9a237fd685e6faaae22d9d8a6a0732065336fc55
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-03-26 01:14:50 +0800

    add note-tree.nvim dev plug

---
+++ lua/plugins/edit.note.tree.nvim.lua
@@ -0,0 +1,9 @@
+return {
+  "pxwg/note-tree.nvim",
+  dev = true,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    { "3rd/image.nvim", lazy = true, build = true }, -- Optional image support in pdf preview
+  },
+  branch = "feature",
+}
