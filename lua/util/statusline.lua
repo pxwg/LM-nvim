2e836dc258c6d561eb72207971f9799a6e626cab
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-03-31 15:57:35 +0800

    update for more effective statusline

--- lua/util/statusline.lua
+++ lua/util/statusline.lua
@@ -15,33 +15,81 @@ local symbols = trouble.statusline({
   hl_group = "StatusLineLang",
 })
 
+-- Cache for expensive operations
+M.cache = {
+  branch = "",
+  branch_time = 0,
+  battery_icon = "",
+  battery_time = 0,
+}
+
+local function get_branch()
+  local current_time = vim.loop.now()
+  -- Update git branch at most every 5 seconds
+  if current_time - M.cache.branch_time > 5000 then
+    local branch =
+      vim.fn.system("git -C " .. vim.fn.expand("%:p:h") .. " branch --show-current 2>/dev/null | tr -d '\n'")
+    if vim.v.shell_error == 0 and branch ~= "" then
+      M.cache.branch = " " .. branch
+    else
+      M.cache.branch = ""
+    end
+    M.cache.branch_time = current_time
+  end
+  return M.cache.branch
+end
+
+local Job = require("plenary.job")
+local branch = ""
+Job:new({
+  command = "git",
+  args = { "branch", "--show-current" },
+  cwd = vim.fn.expand("%:p:h"),
+  on_exit = function(j, _)
+    branch = j:result()[1] or ""
+  end,
+}):sync()
+
 vim.o.statusline = table.concat({
   "%#StatusLineFile#%f", -- 文件名
   "%#StatusLineModified#%m", -- 修改标志
-  -- "%=%{%v:lua.require'util.current_function'.get_current_function()%}%=",
   "%=",
   symbols.get(),
   "%=",
+  "%#StatusLineGit#" .. branch .. " ",
   "%#StatusLineLang#[" .. "en" .. "] ",
   "%#StatusLineBattery#" .. require("util.battery").get_battery_icon() .. " ",
   "%#Normal#",
 })
 
 function M.update_hl()
+  local current_time = vim.loop.now()
+  -- Update battery info at most every 30 seconds
+  if current_time - M.cache.battery_time > 30000 then
+    M.cache.battery_icon = require("util.battery").get_battery_icon()
+    M.cache.battery_time = current_time
+  end
+  local Job = require("plenary.job")
+  local branch = ""
+  Job:new({
+    command = "git",
+    args = { "branch", "--show-current" },
+    cwd = vim.fn.expand("%:p:h"),
+    on_exit = function(j, _)
+      branch = j:result()[1] or ""
+    end,
+  }):sync()
+
   vim.o.statusline = table.concat({
     "%#StatusLineFile#%f", -- 文件名
     "%#StatusLineModified#%m", -- 修改标志
-    -- "%=%{%v:lua.require'util.current_function'.get_current_function()%}%=",
     "%=",
     symbols.get(),
     "%=",
-    -- "%#StatusLineLang#".. require("cn)
-    "%#StatusLineLang#"
-      .. "["
-      .. require("util.rime_ls").rime_toggle_word()
-      .. "] ",
-    "%#StatusLineBattery#" .. require("util.battery").get_battery_icon() .. " ",
-    "%#Normal#", -- Reset highlight at the end(
+    "%#StatusLineGit#" .. branch .. " ",
+    "%#StatusLineLang#" .. "[" .. require("util.rime_ls").rime_toggle_word() .. "] ",
+    "%#StatusLineBattery#" .. M.cache.battery_icon .. " ",
+    "%#Normal#", -- Reset highlight at the end
   })
   require("util.rime_ls").change_cursor_color()
 end
