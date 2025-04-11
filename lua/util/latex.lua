be146ca67c6b305e0ac76b7895510676e7045263
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-04-11 15:52:26 +0800

    feat: treesitter highlight

--- lua/util/latex.lua
+++ lua/util/latex.lua
@@ -10,9 +10,9 @@ local MATH_NODES = {
 
 local ts_utils = require("nvim-treesitter.ts_utils")
 
-M.test = function()
-  return true
-end
+-- M.test = function()
+--   return true
+-- end
 M.in_env = function(env)
   local node = ts_utils.get_node_at_cursor()
   local bufnr = vim.api.nvim_get_current_buf()
@@ -29,40 +29,32 @@ M.in_env = function(env)
   return false
 end
 --
--- M.in_text = function()
---   local node = ts_utils.get_node_at_cursor()
---   while node do
---     if node:type() == "text_mode" then
---       return true
---     elseif MATH_NODES[node:type()] then
---       return false
---     end
---     node = node:parent()
---   end
---   return true
--- end
---
--- M.in_mathzone = function()
---   return not M.in_text()
--- end
+M.in_text = function()
+  local node = ts_utils.get_node_at_cursor()
+  while node do
+    if node:type() == "text_mode" then
+      return true
+    elseif MATH_NODES[node:type()] then
+      return false
+    end
+    node = node:parent()
+  end
+  return true
+end
+
+M.in_mathzone = function()
+  return not M.in_text()
+end
 
 -- environment detection via vimtex
 
-M.in_mathzone = function()
-  return vim.fn["vimtex#syntax#in_mathzone"]() == true
-  -- local node = ts_utils.get_node_at_cursor()
-  -- local bufnr = vim.api.nvim_get_current_buf()
-  -- while node do
-  --   if node:type() == "math_environment" then
-  --     return true
-  --   end
-  -- end
-  -- return false
-end
+-- M.in_mathzone = function()
+--   return vim.fn["vimtex#syntax#in_mathzone"]() == true
+-- end
 
-M.in_text = function()
-  return not M.in_mathzone()
-end
+-- M.in_text = function()
+--   return not M.in_mathzone()
+-- end
 
 M.in_item = function()
   return M.in_env("itemize") or M.in_env("enumerate")
--- queries/latex/highlights.scm
+++ queries/latex/highlights.scm
@@ -354,3 +354,22 @@
 ((line_comment) @keyword.directive @nospell
   (#lua-match? @keyword.directive "^%%&"))
 
+; Conceal the command part
+((command_name) @_cmd
+ (#any-of? @_cmd "\\mathrm" "\\mathit" "\\textit" "\\mathbf" "\\textbf" "\\emph" ) 
+ (#set! conceal ""))
+
+; Conceal the opening brace
+((generic_command
+  command: (command_name) @_cmd
+  arg: (curly_group "{" @_open_brace))
+ (#any-of? @_cmd "\\mathrm" "\\mathit" "\\textit" "\\mathbf" "\\textbf" "\\emph")
+ (#set! @_open_brace conceal ""))
+
+; Conceal the closing brace
+((generic_command
+  command: (command_name) @_cmd
+  arg: (curly_group "}" @_close_brace))
+ (#any-of? @_cmd "\\mathrm" "\\mathit" "\\textit" "\\mathbf" "\\textbf" "\\emph")
+ (#set! @_close_brace conceal ""))
+;
