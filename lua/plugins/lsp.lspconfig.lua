7981345519d115e170730ae887d53407e56db0f9
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-04-13 02:28:51 +0800

    fix: edit lsp

--- lua/plugins/lsp.lspconfig.lua
+++ lua/plugins/lsp.lspconfig.lua
@@ -39,23 +39,10 @@ return {
       capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
       capabilities.general.positionEncodings = { "utf-8", "utf-16" }
 
-      vim.lsp.config.signature_help = {
-        border = "rounded",
-      }
       -- Load mason_lspconfig
       require("mason-lspconfig").setup_handlers({
         function(server_name)
-          -- if server_name.workspace_folders then
-          --   local path = server_name.workspace_folders[1].name
-          --   if
-          --     path ~= vim.fn.stdpath("config")
-          --     and (vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc"))
-          --   then
-          --     return
-          --   end
-          -- end
           require("lspconfig")[server_name].setup({
-            -- offset_encoding = "utf-8", -- wtf? if not set, it shows warning
             capabilities = capabilities,
             root_dir = function(fname)
               local dir = require("util.cwd_attach").get_cwd(fname)
@@ -65,6 +52,9 @@ return {
               Lua = {
                 workspace = {
                   library = vim.api.nvim_get_runtime_file("", true),
+                },
+                runtime = {
+                  version = "LuaJIT",
                 },
                 diagnostics = {
                   globals = { "hs" },
