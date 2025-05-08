8e916981d1d1d5421a1ba7e241bbd15f434f900b
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-05-08 08:55:58 +0800

    feat: disable image.nvim

--- lua/plugins/edit.image.lua
+++ lua/plugins/edit.image.lua
@@ -35,7 +35,7 @@ return {
             -- Make sure you have an html treesitter parser installed
             -- ~/github/dotfiles-latest/neovim/nvim-lazyvim/lua/plugins/treesitter.lua
             html = {
-              enabled = true,
+              enabled = false,
             },
             -- This is disabled by default
             -- Detect and render images referenced in CSS files
@@ -70,5 +70,42 @@ return {
         })
       end
     end,
+  },
+  {
+    "3rd/diagram.nvim",
+    enabled = false,
+    dependencies = {
+      "3rd/image.nvim",
+    },
+    opts = { -- you can just pass {}, defaults below
+      events = {
+        render_buffer = { "InsertLeave", "BufWinEnter", "TextChanged" },
+        clear_buffer = { "BufLeave" },
+      },
+      renderer_options = {
+        mermaid = {
+          background = nil, -- nil | "transparent" | "white" | "#hex"
+          theme = nil, -- nil | "default" | "dark" | "forest" | "neutral"
+          scale = 1, -- nil | 1 (default) | 2  | 3 | ...
+          width = nil, -- nil | 800 | 400 | ...
+          height = nil, -- nil | 600 | 300 | ...
+        },
+        plantuml = {
+          charset = nil,
+        },
+        d2 = {
+          theme_id = nil,
+          dark_theme_id = nil,
+          scale = nil,
+          layout = nil,
+          sketch = nil,
+        },
+        gnuplot = {
+          size = nil, -- nil | "800,600" | ...
+          font = nil, -- nil | "Arial,12" | ...
+          theme = nil, -- nil | "light" | "dark" | custom theme string
+        },
+      },
+    },
   },
 }
