d35e6e37ab6a8cebb4bc2656e958b21eb50c310b
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-07-16 01:36:21 +0800

    feat: update for typst

--- lua/plugins/lsp.lspconfig.lua
+++ lua/plugins/lsp.lspconfig.lua
@@ -17,7 +17,6 @@ return {
     "neovim/nvim-lspconfig",
     event = { "VeryLazy", "BufEnter", "BufReadPost", "BufWritePost", "BufNewFile" },
     version = "^1.0.0",
-    event = { "LazyFile" },
     cmd = "LspStart",
     dependencies = {
       -- Setup lsp installed in mason
@@ -30,6 +29,39 @@ return {
         windows = {
           default_options = {
             border = "rounded",
+          },
+        },
+      },
+      servers = {
+        lua_ls = {
+          -- mason = false, -- set to false if you don't want this server to be installed with mason
+          -- Use this to add any additional keymaps
+          -- for specific lsp servers
+          -- ---@type LazyKeysSpec[]
+          -- keys = {},
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              codeLens = {
+                enable = true,
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+              doc = {
+                privateName = { "^_" },
+              },
+              hint = {
+                enable = true,
+                setType = false,
+                paramType = true,
+                paramName = "Disable",
+                semicolon = "Disable",
+                arrayIndex = "Disable",
+              },
+            },
           },
         },
       },
@@ -51,39 +83,31 @@ return {
               return dir
             end,
           }
-
           -- Special configuration for lua_ls
-          if server_name == "lua_ls" then
-            server_config.settings = {
-              Lua = {
-                workspace = {
-                  library = vim.list_extend(vim.api.nvim_get_runtime_file("", true), {
-                    vim.fn.stdpath("config") .. "/lua",
-                    "${3rd}/luv/library",
-                    vim.fn.expand("HOME") .. "/.hammerspoon/Spoons/EmmyLua.spoon/annotations",
-                  }),
-                },
-                runtime = {
-                  version = "LuaJIT",
-                  path = vim.list_extend(vim.split(package.path, ";"), {
-                    "lua/?.lua",
-                    "lua/?/init.lua",
-                    vim.fn.stdpath("config") .. "/lua/?.lua",
-                    vim.fn.stdpath("config") .. "/lua/?/init.lua",
-                    "${3rd}/luv/library/?.lua",
-                  }),
-                },
-                diagnostics = {
-                  -- globals = { "hs", "vim" },
-                  -- disable = { "missing-fields" },
-                },
-                completion = {
-                  callSnippet = "Replace",
-                },
+          server_config.settings = {
+            Lua = {
+              workspace = {
+                library = vim.list_extend(vim.api.nvim_get_runtime_file("", true), {
+                  vim.fn.stdpath("config") .. "/lua",
+                  "${3rd}/luv/library",
+                  vim.fn.expand("HOME") .. "/.hammerspoon/Spoons/EmmyLua.spoon/annotations",
+                }),
               },
-            }
-          end
-
+              runtime = {
+                version = "LuaJIT",
+                path = vim.list_extend(vim.split(package.path, ";"), {
+                  "lua/?.lua",
+                  "lua/?/init.lua",
+                  vim.fn.stdpath("config") .. "/lua/?.lua",
+                  vim.fn.stdpath("config") .. "/lua/?/init.lua",
+                  "${3rd}/luv/library/?.lua",
+                }),
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
+          }
           require("lspconfig")[server_name].setup(server_config)
         end,
       })
@@ -145,6 +169,57 @@ return {
 
       local pyright_capabilities = vim.lsp.protocol.make_client_capabilities()
       pyright_capabilities = require("blink.cmp").get_lsp_capabilities(pyright_capabilities)
+
+      capabilities.general.positionEncodings = { "utf-8" }
+
+      local rime_on_attach = function(client, _)
+        local toggle_rime = function()
+          client.request("workspace/executeCommand", { command = "rime-ls.toggle-rime" }, function(_, result, ctx, _)
+            if ctx.client_id == client.id then
+              vim.g.rime_enabled = result
+            end
+          end)
+        end
+        -- keymaps for executing command
+        vim.keymap.set("n", "<leader>rr", toggle_rime, { desc = "Toggle [R]ime" })
+        -- vim.keymap.set("i", "jn", toggle_rime, { desc = "Toggle Rime" })
+        vim.keymap.set("n", "<leader>rs", function()
+          vim.lsp.client:exec_cmd({ title = "rime-ls.sync-user-data", command = "rime-ls.sync-user-data" })
+        end, { desc = "[R]ime [S]ync" })
+        -- vim.keymap.set("i", "jn", function()
+        --   require("lsp.rime_ls").toggle_rime()
+        --   _G.rime_toggled = not _G.rime_toggled
+        --   _G.rime_ls_active = not _G.rime_ls_active
+        -- end, { noremap = true, silent = true, desc = "toggle rime-ls" })
+      end
+
+      local rime_ls_filetypes = { "vimwiki", "tex", "markdown", "copilot-chat", "Avante", "codecompanion", "typst" }
+      lspconfig.rime_ls.setup({
+        settings = {
+          name = "rime_ls",
+          single_file_support = true,
+          autostart = true, -- Add this line to prevent automatic start, in order to boost
+          filetypes = rime_ls_filetypes,
+          enabled = vim.g.rime_enabled,
+          cmd = { "rime-ls" },
+          shared_data_dir = "/Library/Input Methods/Squirrel.app/Contents/SharedSupport",
+          user_data_dir = "~/Library/Rime_2/",
+          log_dir = vim.fn.expand("~/.local/share/rime-ls-1/"),
+          paging_characters = { ",", "." },
+          trigger_characters = {},
+          schema_trigger_character = "&",
+          always_incomplete = false,
+          preselect_first = false,
+          show_filter_text_in_label = false,
+          max_candidates = 9,
+          max_tokens = 0,
+          long_filter_text = true,
+          -- long_filter_text = false,
+        },
+        -- on_attach = attach_in_insert_mode,
+        on_attach = rime_on_attach,
+        capabilities = capabilities,
+      })
 
       lspconfig.pyright.setup({
         capabilities = pyright_capabilities,
