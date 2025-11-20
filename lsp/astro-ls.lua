877bc40634628d4bce1e181d5bcc7a3c8925be99
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-11-20 10:13:15 +0800

    fix: correct astro lsp config

--- lsp/astro-ls.lua
+++ lsp/astro-ls.lua
@@ -1,15 +1,30 @@
-local util = require("lspconfig.util")
+-- .config/nvim/lsp/astro.lua
+local function get_tsserver_path(root_dir)
+  local project_root = vim.fs.dirname(vim.fs.find("node_modules", { path = root_dir, upward = true })[1])
+  return project_root and vim.fs.joinpath(project_root, "node_modules", "typescript", "lib") or ""
+end
+
 return {
-  name = "astro-ls",
-  cmd = { "astro-ls", "--stdio" },
-  filetypes = { "astro" },
-  root_dir = vim.fn.getcwd(),
+  cmd = {
+    "astro-ls",
+    "--stdio",
+  },
+  filetypes = {
+    "astro",
+    "ts",
+  },
+  root_markers = {
+    "package.json",
+    "tsconfig.json",
+    "jsconfig.json",
+    ".git",
+  },
   init_options = {
     typescript = {},
   },
-  on_new_config = function(new_config, new_root_dir)
-    if vim.tbl_get(new_config.init_options, "typescript") and not new_config.init_options.typescript.tsdk then
-      new_config.init_options.typescript.tsdk = util.get_typescript_server_path(new_root_dir)
+  before_init = function(_, config)
+    if not config.init_options.typescript.tsdk then
+      config.init_options.typescript.tsdk = get_tsserver_path(config.root_dir)
     end
   end,
 }
