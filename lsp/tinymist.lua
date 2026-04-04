f5710a1c3cebce566bc792766a29ec4121358dcb
Xinyu Xiang <xiangxy23@mails.tsinghua.edu.cn>
2026-04-04 08:14:56 +0800

    fix: remove debug print

--- lsp/tinymist.lua
+++ lsp/tinymist.lua
@@ -2,7 +2,6 @@ local function tinymist_on_attach(client, bufnr)
   if not client or client.name ~= "tinymist" then
     return
   end
-  print("Tinymist LSP attached to buffer " .. bufnr)
   local bufname = vim.api.nvim_buf_get_name(bufnr)
   local root_markers = client.config.root_markers or {}
   local root = require("lspconfig.util").root_pattern(unpack(root_markers))(bufname) or vim.fs.dirname(bufname)
