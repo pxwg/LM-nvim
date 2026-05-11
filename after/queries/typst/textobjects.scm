d68d46949d051b4b70876ca226eea77d0e15c29a
Xinyu Xiang <xiangxy23@mails.tsinghua.edu.cn>
2026-05-11 19:39:39 +0800

    fix: remove textobjects for typst for now

--- after/queries/typst/textobjects.scm
+++ after/queries/typst/textobjects.scm
@@ -1,37 +1,37 @@
 ; extends
 
 ; Label references (@xxxx)
-(ref) @reference.outer
-
-((math
-  "$"
-  (_) @_start
-  (_)? @_end
-  "$") @math.outer)
-
-((math
-  "$"
-  ([_] @math.inner)* 
-  "$"))
-
-; extends
-
-; General section with form ==... xxx
+; (ref) @reference.outer
+;
+; ((math
+;   "$"
+;   (_) @_start
+;   (_)? @_end
+;   "$") @math.outer)
+;
+; ((math
+;   "$"
+;   ([_] @math.inner)* 
+;   "$"))
+;
+; ; extends
+;
+; ; General section with form ==... xxx
+; ; ((section
+; ;   ((heading) @head_operator
+; ;     (#match? @head_operator "^=\\s+.*"))
+; ;   .
+; ;   (_) @start
+; ;   (_)? @end .) @section.outer
+; ;   (#make-range! "section.inner" @start @end))
+;
 ; ((section
-;   ((heading) @head_operator
-;     (#match? @head_operator "^=\\s+.*"))
+;   (heading 
+;     "=" @head_operator)
 ;   .
 ;   (_) @start
 ;   (_)? @end .) @section.outer
 ;   (#make-range! "section.inner" @start @end))
-
-((section
-  (heading 
-    "=" @head_operator)
-  .
-  (_) @start
-  (_)? @end .) @section.outer
-  (#make-range! "section.inner" @start @end))
 
 ; ((section((heading) @head_operator
 ;          (#match? @head_operator "^=\\s+.*"))
